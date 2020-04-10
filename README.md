12b38d9f8c81f9f2677ead6268747ac9532b46b6
rashikshrestha <073bex432.rashik@pcampus.edu.np>
2020-04-10 20:34:01 +0545

    autocomplete updated

--- README.md
+++ README.md
@@ -14,11 +14,11 @@ In Terminal inside 'rsk_ws' folder:
 ```
 catkin_make
 sudo ./init_rsk_ws.sh
-
 ```
 
 #### Step 4:
 In Terminal at any directory:
 ```
 gimme_a_world iscas_museum
+gimme_a_bot stereo_bot
 ```
--- rsk_ws_autocomplete.bash
+++ rsk_ws_autocomplete.bash
@@ -3,3 +3,6 @@
 world_list=$(ls $RSK_WS/src/rsk_world/worlds/ | sed -e 's/\.world$//')
 complete -W "$world_list" gimme_a_world
 
+bot_list=$(ls $RSK_WS/src/rsk_bot/urdf/ -F | grep \/$ | sed -e 's/\/$//')
+complete -W "$bot_list" gimme_a_bot
+
